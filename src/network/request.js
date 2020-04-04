e6666db3a82d603d070dacfd5148f56e842a2ffb
oscarliusy <1069176850@qq.com>
2020-04-04 19:55:30 +0800

    category

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export const request=(config)=>{
   const instance = axios.create({
     //baseURL: 'http://123.207.32.32:8000',
     //baseURL: 'http://106.54.54.237:8000/api/wh',
-    baseURL: 'http://123.207.32.32:8000/api/h3',
+    baseURL: 'http://123.207.32.32:8000/api/h8',
     timeout: 5000
   })
 
@@ -21,4 +21,25 @@ export const request=(config)=>{
   })
 
   return instance(config)
+}
+
+export const requestRap2 = (config) =>{
+  const instanceRap2 = axios.create({
+    baseURL:'http://rap2.taobao.org:38080/app/mock/249676',
+    timeout: 5000
+  })
+
+  instanceRap2.interceptors.request.use(config=>{
+    return config
+  },err =>{
+
+  })
+
+  instanceRap2.interceptors.response.use(res=>{
+    return res.data.data
+  },err =>{
+    console.log(err)
+  })
+
+  return instanceRap2(config)
 }
---
+++ src/views/category/childComps/CategoryVerticalTabBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="vertical">
+    <div v-for="(item,index) in tags" :key="index">{{item}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'CategoryVerticalTabBar',
+  data(){
+    return {
+      tags:['aaa','bbb','ccc','ddd','eee','fff','aaa','bbb','ccc','ddd','eee','fff']
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
